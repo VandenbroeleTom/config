e23bb304997ff4f47f51f29009b202f1225cf6a0
Tom Vandenbroele <vandenbroeletom@gmail.com>
2021-10-23 22:33:33 +0200

    html & css lsp

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -37,7 +37,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'intelephense' }
+local servers = { 'intelephense', 'html', 'cssls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup(coq.lsp_ensure_capabilities({
     on_attach = on_attach,
